e07e5d76f1beae10a2862245c3233062beb2fe87
shiv <sdeshmukh@tavisca.com>
2015-07-18 09:51:16 +0530

    defining relationship

--- EFAssignment/Model/UserPermissions.cs
+++ EFAssignment/Model/UserPermissions.cs
@@ -8,7 +8,28 @@ namespace EFAssignment.Model
 {
     public class UserPermissions
     {
+        private ICollection<Users> _users;
+        private ICollection<Permissions> _permissions;
+
+        public UserPermissions()
+        {
+            _users = new List<Users>();
+            _permissions = new List<Permissions>();
+        }
+
         public int UserId { get; set; }
         public int PermissionId { get; set; }
+
+        public virtual ICollection<Users> Users
+        {
+            get { return _users; }
+            set { _users = value; }
+        }
+
+        public virtual ICollection<Permissions> Permissions
+        {
+            get { return _permissions; }
+            set { _permissions = value; }
+        }
     }
 }
