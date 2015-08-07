9e19003c47f9c264e6c7da94e310ffb39b9c2a77
Shiv <sdeshmukh@tavisca.com>
2015-08-07 14:51:59 +0530

    added test case project

--- EFAssignment/Program.cs
+++ EFAssignment/Program.cs
@@ -8,22 +8,15 @@ namespace EFAssignment
     {
         static void Main(string[] args)
         {
-            using (var db = new UserDbContext())
-            {
-                //db.Users.Add(new Users()
-                //{
-                //    Id = 1,
-                //    FirstName = "Shiv",
-                //    LastName = "Deshmukh"
-                //});
-                //db.SaveChanges();
-                var list = db.Users.ToList();
-                foreach (var item in list)
-                {
-                    Console.WriteLine(item.FirstName);
-                    Console.ReadLine();
-                }
-            }
+            //using (var db = new UserDbContext())
+            //{
+            //    //var list = db.Users.ToList();
+            //    //foreach (var item in list)
+            //    //{
+            //    //    Console.WriteLine(item.FirstName);
+            //    //    Console.ReadLine();
+            //    //}
+            //}
         }   
     }
 }
---
+++ UnitTestProject/UnitTest.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Linq;
+using DbModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void Read_Users_Top10()
+        {
+            using (var db = new UserDbContext())
+            {                
+                var users = db.Users.Take(10).ToList();
+
+                Assert.AreEqual(10, users.Count);
+
+                foreach (var user in users)
+                {
+                    Assert.IsNotNull(user.FirstName);
+                }
+            }
+        }
+    }
+}
